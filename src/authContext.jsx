4558bde458cd89ef1f12d1c45ba018c3dd227402
Mansi Mehra <mansi2004mehra18@gmail.com>
2025-06-06 21:34:36 +0530

    first commit

---
+++ src/authContext.jsx
@@ -0,0 +1,29 @@
+import React, {
+  createContext,
+  useState,
+  useEffect,
+  useContext,
+  Children,
+} from "react";
+
+const AuthContext = createContext();
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export const AuthProvider = ({ Children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  useEffect(() => {
+    const userId = localStorage.getItem("userId");
+    if(userId) {
+        setCurrentUser(userId);
+    }
+  }, []);
+
+  const value = {
+    currentUser, setCurrentUser
+  }
+  
+  return <AuthContext.Provider value={value}>{Children}</AuthContext.Provider>
+};
